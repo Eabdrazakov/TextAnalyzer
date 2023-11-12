e09efc52ba54f1f93c0abe6561c59a46871f15d2
Padma Patil <padma@email.com>
2023-11-12 13:07:37 -0800

    Add function

--- js/scrips.js
+++ js/scrips.js
@@ -54,3 +54,23 @@ function boldPassage(word, text) {
     });
     return p;
 }
+
+
+function handleFormSubmission(event) {
+    event.preventDefault();
+    const passage = document.getElementById("text-passage").value;
+    const word = document.getElementById("word").value;
+    const wordCount = wordCounter(passage);
+    const occurrencesOfWord = numberOfOccurrencesInText(word, passage);
+
+    document.getElementById("total-count").innerText = wordCount;
+    document.getElementById("selected-count").innerText = occurrencesOfWord;
+
+    let boldedPassage = boldPassage(word, passage);
+    if (boldedPassage) {
+        document.querySelector("div#bolded-passage").append(boldedPassage);
+    } else {
+        document.querySelector("div#bolded-passage").innerText = null;
+    }
+}
+
